ab75e174bee27995316eee11a7f78166ab70914d
jinusong <sugwer7464@gmail.com>
2018-08-25 21:48:10 +0900

    var / val

---
+++ Start Kotlin/variable.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    // 값 및 변수 선언
+
+    // String 타입을 갖는 값 a 선언
+    var a : String = "foo"
+
+    // 할당하는 자료의 타입에 따라 자동으로 타입을 추론하므로 타입을 생략할 수 있습니다.
+    val b = "bar"
+
+    // 변수 선언 시 자료를 할당해야 합니다.
+    val c: String
+
+    // 자료 할당
+    c = "baz"
+
+    // 변수 d 선언
+    var d: Int = 0
+
+    // 변수 값 변경
+    d += 1
+}
